e1f3150e6dc306ca39b5482f84bc8c2c9dc8e07e
RaFl91 <Terrell.Flemings@gmail.com>
2018-10-30 09:30:39 -0400

    completed db/ - both seeds and connection file filled; pre seeded db

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect
+mongoose.connect('mongodb://localhost/WDI18-p2-pebble')
+
+mongoose.connection.once('open', () => {
+    console.log(`Mongoose has connected to MongoDB`)
+})
+mongoose.connection.on('error', (error) => {
+    console.error(`MongoDB connection error!!! ${error}`)
+    process.exit(-1)
+})
+
+module.exports = mongoose
