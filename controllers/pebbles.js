f19100a1b6710613e6e2d395f1640d061587e038
RaFl91 <Terrell.Flemings@gmail.com>
2018-10-30 20:30:32 -0400

    init routes/ controllers

--- controllers/pebbles.js
+++ controllers/pebbles.js
@@ -7,12 +7,24 @@ const pebblesController = {
     index: (req, res) => {
         // res.send("Hey whats up this is app index")
         res.render('app/index')
+        Pebble.find().populate('powers')
+            .then((pebbles) => {
+                res.render('pebbles/index', {
+                    pebbles: pebbles
+                })
+            })
     },
     new: (req, res) => {
         res.send(`This is user new page`)
     },
     show: (req, res) => {
-        res.send(`This is user show page`)
+        // res.send(`This is user show page`)
+        Pebble.findById(pebbleId).populate('powers')
+            .then((pebble) => {
+                res.render('pebble/show', {
+                    pebble: pebble
+                })
+            })
     }
 }
 
--- controllers/users.js
+++ controllers/users.js
@@ -7,14 +7,23 @@ const Power = require('../models/Power')
 const usersController = {
     index: (req, res) => {
         // res.send("Hey whats up this is app index")
-        res.render('app/index')
-        
-    }, 
+        // res.render('app/index')
+        User.find().populate('pebbles')
+            .then((users) => {
+                res.render('users/index', {
+                    users: users
+                })
+            })
+    },
     new: (req, res) => {
         res.send(`This is user new page`)
     },
     show: (req, res) => {
-        res.send(`This is user show page`)
+        // res.send(`This is user show page`)
+        User.findById(userId).populate('pebbles')
+        .then((user) => {
+            res.render('user/show', {user: user})
+        })
     }
 }
 
